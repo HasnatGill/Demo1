7b17159feee88b929f4cf01bd3be5634250a01cd
HasnatGill <hasnatgill404@gmail.com>
2022-11-21 17:52:45 +0500

    Working on home page

--- src/pages/Frontend/Home/Home.js
+++ src/pages/Frontend/Home/Home.js
@@ -1,11 +1,17 @@
 import React from "react";
-import Header from '../../component/Header'
-
+import Header from "../../component/Header";
 
 function Home() {
   return (
     <>
       <Header />
+      <div className="home">
+        <div className="container">
+        <div className="toppart">
+
+        </div>
+        </div>
+      </div>
     </>
   );
 }
