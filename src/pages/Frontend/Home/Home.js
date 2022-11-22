b93056b93b401851b8f69e41a9f4625c50a96f13
Hasnat Majid <107733484+HasnatGill@users.noreply.github.com>
2022-11-22 13:22:12 +0500

    Changes in Home pages

--- src/pages/Frontend/Home/Home.js
+++ src/pages/Frontend/Home/Home.js
@@ -34,7 +34,7 @@ function Home() {
         </div>
       </div>
       <div className="container my-5">
-        <div className="row ">
+        {/* <div className="row ">
           <div className="col-2">
             <div className="card h-50 position-relative">
               <span
@@ -96,8 +96,239 @@ function Home() {
               <div className="card-body h-50">
                 <h5 className="ms-1">Person Hotel</h5>
                 <p className="mb-0 ms-1">Hotel/stay</p>
-                <p className="mb-0 pt-2"><GoLocation className="text-primary"/> Turkey,Mamaris</p>
-                <p className="mb-0 pt-2"><HiOutlineTicket className="text-primary"/> Ticket available:375</p>
+                <p className="mb-0 pt-2"><GoLocation className="text-primary" /> Turkey,Mamaris</p>
+                <p className="mb-0 pt-2"><HiOutlineTicket className="text-primary" /> Ticket available:375</p>
+              </div>
+            </div>
+          </div>
+        </div> */}
+
+
+        <div className="mt-5">
+          <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="true">
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+              <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span className="visually-hidden">Previous</span>
+            </button>
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+              <span className="carousel-control-next-icon" aria-hidden="true"></span>
+              <span className="visually-hidden">Next</span>
+            </button>
+            <div className="carousel-indicators">
+              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div className="carousel-inner">
+              <div className="carousel-item active">
+            <div className="row ">
+          <div className="col-2">
+            <div className="card h-50 position-relative">
+              <span
+                class="badge text-bg-danger rounded-0 top-3 mt-2 position-absolute"
+                style={{ marginLeft: "-8px" }}
+              >
+                Up to 50% off
+              </span>
+              <img
+                src={Aladdin}
+                className="card-img h-100"
+              ></img>
+              <div className="position-absolute bottom-0 mb-2 ms-4">
+                <h4 className="ms-4 text-white">Aladdin</h4>
+              </div>
+            </div>
+            <div className="card mt-1 position-relative" style={{ height: "49%" }}>
+              <span
+                class="badge text-bg-danger rounded-0 top-3 mt-2 position-absolute"
+                style={{ marginLeft: "-8px" }}
+              >
+                Up to 50% off
+              </span>
+              <img
+                src={dear}
+                className="card-img h-100"
+              ></img>
+              <div className="position-absolute bottom-0 mb-2 ms-1">
+                <h4 className=" text-white text-center">Dear Evan Hansens</h4>
+              </div>
+            </div>
+          </div>
+          <div className="col-8 rounded-2 position-relative bg-img2">
+            <div className="rounded-2">
+              <div className="position-absolute bottom-0 ms-3 mb-2 text-white">
+                <h3><b>Explore our seoul</b></h3>
+                <p>
+                  200 hotels,345 local fights and 234 bus providers
+                </p>
+              </div>
+              <div className="position-absolute end-0 bottom-0 mb-4 me-3">
+                <button className="btn bg-orange text-white">Explore</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-2">
+            <div className="card h-100 border-0">
+              <span
+                class="badge text-bg-danger rounded-0 top-3 mt-2 position-absolute"
+                style={{ marginLeft: "-8px" }}
+              >
+                Up to 50% off
+              </span>
+              <img
+                src="https://images.unsplash.com/photo-1631049307264-da0ec9d70304?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aG90ZWwlMjByb29tfGVufDB8fDB8fA%3D%3D&w=1000&q=80"
+                className="card-img h-75"
+                alt=""
+              />
+              <div className="card-body h-50">
+                <h5 className="ms-1">Person Hotel</h5>
+                <p className="mb-0 ms-1">Hotel/stay</p>
+                <p className="mb-0 pt-2"><GoLocation className="text-primary" /> Turkey,Mamaris</p>
+                <p className="mb-0 pt-2"><HiOutlineTicket className="text-primary" /> Ticket available:375</p>
+              </div>
+            </div>
+          </div>
+        </div>
+              </div>
+              <div className="carousel-item">
+              <div className="row ">
+          <div className="col-2">
+            <div className="card h-50 position-relative">
+              <span
+                class="badge text-bg-danger rounded-0 top-3 mt-2 position-absolute"
+                style={{ marginLeft: "-8px" }}
+              >
+                Up to 50% off
+              </span>
+              <img
+                src={Aladdin}
+                className="card-img h-100"
+              ></img>
+              <div className="position-absolute bottom-0 mb-2 ms-4">
+                <h4 className="ms-4 text-white">Aladdin</h4>
+              </div>
+            </div>
+            <div className="card mt-1 position-relative" style={{ height: "49%" }}>
+              <span
+                class="badge text-bg-danger rounded-0 top-3 mt-2 position-absolute"
+                style={{ marginLeft: "-8px" }}
+              >
+                Up to 50% off
+              </span>
+              <img
+                src={dear}
+                className="card-img h-100"
+              ></img>
+              <div className="position-absolute bottom-0 mb-2 ms-1">
+                <h4 className=" text-white text-center">Dear Evan Hansens</h4>
+              </div>
+            </div>
+          </div>
+          <div className="col-8 rounded-2 position-relative bg-img2">
+            <div className="rounded-2">
+              <div className="position-absolute bottom-0 ms-3 mb-2 text-white">
+                <h3><b>Explore our seoul</b></h3>
+                <p>
+                  200 hotels,345 local fights and 234 bus providers
+                </p>
+              </div>
+              <div className="position-absolute end-0 bottom-0 mb-4 me-3">
+                <button className="btn bg-orange text-white">Explore</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-2">
+            <div className="card h-100 border-0">
+              <span
+                class="badge text-bg-danger rounded-0 top-3 mt-2 position-absolute"
+                style={{ marginLeft: "-8px" }}
+              >
+                Up to 50% off
+              </span>
+              <img
+                src="https://images.unsplash.com/photo-1631049307264-da0ec9d70304?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aG90ZWwlMjByb29tfGVufDB8fDB8fA%3D%3D&w=1000&q=80"
+                className="card-img h-75"
+                alt=""
+              />
+              <div className="card-body h-50">
+                <h5 className="ms-1">Person Hotel</h5>
+                <p className="mb-0 ms-1">Hotel/stay</p>
+                <p className="mb-0 pt-2"><GoLocation className="text-primary" /> Turkey,Mamaris</p>
+                <p className="mb-0 pt-2"><HiOutlineTicket className="text-primary" /> Ticket available:375</p>
+              </div>
+            </div>
+          </div>
+        </div>
+              </div>
+              <div className="carousel-item">
+              <div className="row ">
+          <div className="col-2">
+            <div className="card h-50 position-relative">
+              <span
+                class="badge text-bg-danger rounded-0 top-3 mt-2 position-absolute"
+                style={{ marginLeft: "-8px" }}
+              >
+                Up to 50% off
+              </span>
+              <img
+                src={Aladdin}
+                className="card-img h-100"
+              ></img>
+              <div className="position-absolute bottom-0 mb-2 ms-4">
+                <h4 className="ms-4 text-white">Aladdin</h4>
+              </div>
+            </div>
+            <div className="card mt-1 position-relative" style={{ height: "49%" }}>
+              <span
+                class="badge text-bg-danger rounded-0 top-3 mt-2 position-absolute"
+                style={{ marginLeft: "-8px" }}
+              >
+                Up to 50% off
+              </span>
+              <img
+                src={dear}
+                className="card-img h-100"
+              ></img>
+              <div className="position-absolute bottom-0 mb-2 ms-1">
+                <h4 className=" text-white text-center">Dear Evan Hansens</h4>
+              </div>
+            </div>
+          </div>
+          <div className="col-8 rounded-2 position-relative bg-img2">
+            <div className="rounded-2">
+              <div className="position-absolute bottom-0 ms-3 mb-2 text-white">
+                <h3><b>Explore our seoul</b></h3>
+                <p>
+                  200 hotels,345 local fights and 234 bus providers
+                </p>
+              </div>
+              <div className="position-absolute end-0 bottom-0 mb-4 me-3">
+                <button className="btn bg-orange text-white">Explore</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-2">
+            <div className="card h-100 border-0">
+              <span
+                class="badge text-bg-danger rounded-0 top-3 mt-2 position-absolute"
+                style={{ marginLeft: "-8px" }}
+              >
+                Up to 50% off
+              </span>
+              <img
+                src="https://images.unsplash.com/photo-1631049307264-da0ec9d70304?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aG90ZWwlMjByb29tfGVufDB8fDB8fA%3D%3D&w=1000&q=80"
+                className="card-img h-75"
+                alt=""
+              />
+              <div className="card-body h-50">
+                <h5 className="ms-1">Person Hotel</h5>
+                <p className="mb-0 ms-1">Hotel/stay</p>
+                <p className="mb-0 pt-2"><GoLocation className="text-primary" /> Turkey,Mamaris</p>
+                <p className="mb-0 pt-2"><HiOutlineTicket className="text-primary" /> Ticket available:375</p>
+              </div>
+            </div>
+          </div>
+        </div>
               </div>
             </div>
           </div>
