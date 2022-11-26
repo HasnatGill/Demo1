8bdd9feb933fd0f2ed340e5aa49d99b31a53d179
Hasnat Majid <107733484+HasnatGill@users.noreply.github.com>
2022-11-26 12:26:15 +0500

    working on dashboard

--- src/pages/component/DashTopBar/TopBar.js
+++ src/pages/component/DashTopBar/TopBar.js
@@ -1,11 +1,37 @@
 import React from 'react'
+import { AiOutlineBell } from 'react-icons/ai'
+import Profile from '../../../pics/profile.jpeg'
 
 function TopBar() {
-  return (
-    <div>
-         <h4>This is Dashboard Top Bar.</h4>      
-    </div>
-  )
+    return (
+        <div className='DashBoardTopBar'>
+            <div className="row">
+                <div className="col">
+                    <div className="left">
+                        <h4>Product</h4>
+                    </div>
+                    <div className="right">
+                        <div className="language">
+                            <select >
+                                <option value="EN">EN</option>
+                                <option value="UDR">UDR</option>
+                            </select>
+                        </div>
+                        <div className="icon">
+                            <AiOutlineBell />
+                        </div>
+                        <div className="Profile">
+                            <img src={Profile} className='profile'/>
+                            <h6>Profile</h6>
+                        </div>
+                        <div className="button">
+                            <button>Traveler mode</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default TopBar
