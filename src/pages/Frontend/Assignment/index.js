4c61b5013f2ec90349363e4cf29e3bd4593035b3
Hasnat Majid <107733484+HasnatGill@users.noreply.github.com>
2022-11-30 12:48:06 +0500

    working on dashboard is complete

--- src/pages/Frontend/Assignment/index.js
+++ src/pages/Frontend/Assignment/index.js
@@ -2,11 +2,11 @@ import React from 'react'
 import MainPage from './MainPage'
 
 function index() {
-  return (
-    <div>
-      <MainPage/>
-    </div>
-  )
+    return (
+        <div>
+            <MainPage />
+        </div>
+    )
 }
 
 export default index
