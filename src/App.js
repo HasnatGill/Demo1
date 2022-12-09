22ab9cf702d9279ab227c44809e7c173188d5c30
Hasnat Majid <107733484+HasnatGill@users.noreply.github.com>
2022-12-09 10:09:14 +0500

    Change the code

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 // import SignUp from "./pages/Auth/Signin/SignUp";
 // import Signin from "./pages/Auth/Signin/Signin";
 // import Dashboard from "./pages/Frontend/Dashboard"
-// import Assignment from './pages/Frontend/Assignment'
+import Assignment from './pages/Frontend/Assignment'
 // import Calendar from "./pages/Frontend/Calendar";
 import AddButton from "./pages/Frontend/AddButton";
 
@@ -17,9 +17,9 @@ function App() {
       {/* <Home /> */}
       {/* <Signin /> */}
       {/* <Dashboard /> */}
-      {/* <Assignment /> */}
+      <Assignment />
       {/* <Calendar/> */}
-      <AddButton/>
+      {/* <AddButton/> */}
       {/* <SignUp /> */}
       {/* <Show /> */}
       {/* <Api /> */}
