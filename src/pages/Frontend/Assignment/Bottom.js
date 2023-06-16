1bf253c47a9c57cd63840b253f82594a36168f9c
Hasnat Majid <107733484+HasnatGill@users.noreply.github.com>
2023-06-16 15:58:38 +0500

    Tweaks

--- src/pages/Frontend/Assignment/Bottom.js
+++ src/pages/Frontend/Assignment/Bottom.js
@@ -5,7 +5,7 @@ function Bottom() {
     <div className="BottomContent">
       <div className="row">
         <div className="col mt-3 ms-4">
-          <p><b>US Headquarters:</b> Monday to Friday,24 Hours</p>
+          <p><b>US Headquarters:</b> to Friday,24 Hours</p>
           <p>Sat/Sun/Holiday,EST 09:00-22:00 <b>call:</b> +1 1 212-828-0100</p>
           <p>Closed on Saturdays,Sundays, and Public Holidays <b>Call:</b> 070-8095-1520 </p>
           <p><b>New York Lounge hours</b></p>
